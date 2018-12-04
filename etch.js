81c18ecb70c07a02ebe0e7f6c504875be96746f5
jenkshields <jennifer@jenniferkshields.nz>
2018-12-04 16:34:21 +1300

    first commit - draw functional but having grid issues

---
+++ etch.js
@@ -0,0 +1,41 @@
+const container = document.querySelector('#container');
+const resetButton = document.querySelector('#reset');
+container.style.width = '75%';
+createGrid(256);
+
+resetButton.addEventListener('click', (e) => {reset()});
+
+function createGrid(size) {
+    let long = (size/size);
+    for (var i = 0; i < size; i++) {
+        var gridDiv = document.createElement('div');
+        gridDiv.classList.add('empty');
+        container.style.setProperty(`--rowNum`, long);
+        container.style.setProperty(`--colNum`, long);
+        container.appendChild(gridDiv);
+    }
+    draw();
+}
+
+
+
+function draw() {
+    const empty = document.querySelectorAll('.empty');
+    empty.forEach((cell) => {
+        cell.addEventListener('mouseover', (e) => {
+            cell.classList.add('filled')}
+    )});
+}
+
+function reset() {
+    var size = prompt("Enter new size:");
+    size = Number(size);
+
+    alert(`Creating new grid, ${size} by ${size} large`);
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    size = (size * size);
+    createGrid(size);
+
+}
